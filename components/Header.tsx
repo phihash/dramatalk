901cd12557f07f5d1bead103a2fd34524d1aec57
phihash <insharp0220@gmail.com>
2023-07-14 00:46:32 +0900

    formatかけただけ

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,6 @@ const Header = () => {
     setIsOpen(!isOpen);
   };
 
-
   return (
     <header className="flex items-center justify-between px-10 py-6 bg-white shadow-md">
       <Link href="/" legacyBehavior>
@@ -22,12 +21,20 @@ const Header = () => {
         <ul className="flex items-center space-x-12">
           <li>
             <Link href="/create/thread" legacyBehavior>
-              <a className={`text-gray-800 font-bold border-slate-500 hover:border-b-4`}>新規作成</a>
+              <a
+                className={`text-gray-800 font-bold border-slate-500 hover:border-b-4`}
+              >
+                新規作成
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/search" legacyBehavior>
-              <a className={`text-gray-800 font-bold border-slate-500 hover:border-b-4`}>探す</a>
+              <a
+                className={`text-gray-800 font-bold border-slate-500 hover:border-b-4`}
+              >
+                探す
+              </a>
             </Link>
           </li>
           <li className="bg-blue-600 px-4 py-3 rounded font-bold hover:bg-blue-500">
@@ -59,43 +66,40 @@ const Header = () => {
 
       {isOpen && (
         <>
-
-        <nav className="md:hidden fixed inset-0 flex items-center justify-center bg-white z-10">
-          <ul className="space-y-12 w-1/2">
-            <li>
-              <Link href="/create/thread" legacyBehavior>
-                <button className="block rounded-lg bg-sky-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-sky-400 focus-visible:ring active:bg-gray-600 ">
-                  Create
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Link href="/search" legacyBehavior>
-              <button className="block rounded-lg bg-sky-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-sky-400 focus-visible:ring active:bg-gray-600 ">
-
-
-                  Search
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Link href="/login" legacyBehavior>
-              <button className="block rounded-lg bg-blue-600 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-blue-500 focus-visible:ring active:bg-gray-600 ">
-
-
-                  Log in
-                </button>
-              </Link>
-            </li>
-            <li>
-            <button onClick={toggleMenu} className="block rounded-lg bg-gray-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-gray-400 focus-visible:ring active:bg-gray-600 ">
+          <nav className="md:hidden fixed inset-0 flex items-center justify-center bg-white z-10">
+            <ul className="space-y-12 w-1/2">
+              <li>
+                <Link href="/create/thread" legacyBehavior>
+                  <button className="block rounded-lg bg-sky-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-sky-400 focus-visible:ring active:bg-gray-600 ">
+                    Create
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/search" legacyBehavior>
+                  <button className="block rounded-lg bg-sky-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-sky-400 focus-visible:ring active:bg-gray-600 ">
+                    Search
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/login" legacyBehavior>
+                  <button className="block rounded-lg bg-blue-600 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-blue-500 focus-visible:ring active:bg-gray-600 ">
+                    Log in
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={toggleMenu}
+                  className="block rounded-lg bg-gray-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-gray-400 focus-visible:ring active:bg-gray-600 "
+                >
                   Close
                 </button>
-            </li>
-          </ul>
-        </nav>
+              </li>
+            </ul>
+          </nav>
         </>
-
       )}
     </header>
   );
