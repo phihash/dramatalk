2bcd0836960572e9babfe24410ad41cb7930f08d
phihash <insharp0220@gmail.com>
2023-07-16 01:25:12 +0900

    フォーマットかけた

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,26 +1,22 @@
-import React from 'react'
+import React from "react";
 
 interface CategoryProps {
   categoryName: string;
 }
 
-const Category:React.FC<CategoryProps>  = ({categoryName}) => {
+const Category: React.FC<CategoryProps> = ({ categoryName }) => {
   return (
-    <div  className="w-5/12 text-center">
-                <input
-                  type="checkbox"
-                  id={categoryName}
-                  className="peer hidden"
-                />
-                <label
-                  htmlFor={categoryName}
-                  className="text-sm md:text-base select-none cursor-pointer rounded-lg border-2 border-sky-200 bg-sky-200 block
+    <div className="w-5/12 text-center">
+      <input type="checkbox" id={categoryName} className="peer hidden" />
+      <label
+        htmlFor={categoryName}
+        className="text-sm md:text-base select-none cursor-pointer rounded-lg border-2 border-sky-200 bg-sky-200 block
    py-3 px-6 font-bold text-white transition-colors duration-200 ease-in-out peer-checked:bg-sky-500 peer-checked:text-white peer-checked:border-sky-500 "
-                >
-                  {categoryName}
-                </label>
-     </div>
-  )
-}
+      >
+        {categoryName}
+      </label>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
