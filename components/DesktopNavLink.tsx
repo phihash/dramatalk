b16e5e5048e05acec228ec586963ae07c1c49405
phihash <insharp0220@gmail.com>
2023-07-17 13:27:12 +0900

    デスクトップの時のナビゲーションリンクコンポーネントを作成si

---
+++ components/DesktopNavLink.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+
+type Prop = {
+  listItemName: string;
+  className: string;
+  href: string;
+};
+
+const DesktopNavLink = ({ listItemName, className, href }: Prop) => {
+  return (
+    <li>
+      <Link href={href} legacyBehavior>
+        <a className={className}>{listItemName}</a>
+      </Link>
+    </li>
+  );
+};
+
+export default DesktopNavLink;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import DesktopNavLink from "./DesktopNavLink";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +11,6 @@ const Header = () => {
     setIsOpen(!isOpen);
   };
 
-
   return (
     <header className="flex items-center justify-between px-10 py-6 bg-white shadow-md">
       <Link href="/" legacyBehavior>
@@ -20,21 +19,21 @@ const Header = () => {
 
       <nav className="hidden md:block">
         <ul className="flex items-center space-x-12">
-          <li>
-            <Link href="/create" legacyBehavior>
-              <a className={`text-gray-800 font-bold border-slate-500 hover:border-b-4`}>新規作成</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/search" legacyBehavior>
-              <a className={`text-gray-800 font-bold border-slate-500 hover:border-b-4`}>探す</a>
-            </Link>
-          </li>
-          <li className="bg-blue-600 px-4 py-3 rounded font-bold hover:bg-blue-500">
-            <Link href="/login" legacyBehavior>
-              <a className={`text-white`}>Log in</a>
-            </Link>
-          </li>
+          <DesktopNavLink
+            href="/create"
+            listItemName="新規作成"
+            className="text-gray-800 font-bold border-slate-500 hover:border-b-4"
+          />
+          <DesktopNavLink
+            href="/search"
+            listItemName="探す"
+            className="text-gray-800 font-bold border-slate-500 hover:border-b-4"
+          />
+          <DesktopNavLink
+            href="/login"
+            listItemName="ログイン"
+            className="text-white bg-blue-600 px-4 py-3 rounded font-bold hover:bg-blue-500"
+          />
         </ul>
       </nav>
 
@@ -59,43 +58,40 @@ const Header = () => {
 
       {isOpen && (
         <>
-
-        <nav className="md:hidden fixed inset-0 flex items-center justify-center bg-white z-10">
-          <ul className="space-y-12 w-1/2">
-            <li>
-              <Link href="/create" legacyBehavior>
-                <button className="block rounded-lg bg-sky-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-sky-400 focus-visible:ring active:bg-gray-600 ">
-                  Create
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Link href="/search" legacyBehavior>
-              <button className="block rounded-lg bg-sky-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-sky-400 focus-visible:ring active:bg-gray-600 ">
-
-
-                  Search
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Link href="/login" legacyBehavior>
-              <button className="block rounded-lg bg-blue-600 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-blue-500 focus-visible:ring active:bg-gray-600 ">
-
-
-                  Log in
-                </button>
-              </Link>
-            </li>
-            <li>
-            <button onClick={toggleMenu} className="block rounded-lg bg-gray-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-gray-400 focus-visible:ring active:bg-gray-600 ">
+          <nav className="md:hidden fixed inset-0 flex items-center justify-center bg-white z-10">
+            <ul className="space-y-12 w-1/2">
+              <li>
+                <Link href="/create" legacyBehavior>
+                  <button className="block rounded-lg bg-sky-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-sky-400 focus-visible:ring active:bg-gray-600 ">
+                    Create
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/search" legacyBehavior>
+                  <button className="block rounded-lg bg-sky-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-sky-400 focus-visible:ring active:bg-gray-600 ">
+                    Search
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/login" legacyBehavior>
+                  <button className="block rounded-lg bg-blue-600 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-blue-500 focus-visible:ring active:bg-gray-600 ">
+                    Log in
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={toggleMenu}
+                  className="block rounded-lg bg-gray-500 w-full px-16 py-4 text-center text-sm font-bold text-white  text-lg outline-none ring-gray-300 transition duration-100 hover:bg-gray-400 focus-visible:ring active:bg-gray-600 "
+                >
                   Close
                 </button>
-            </li>
-          </ul>
-        </nav>
+              </li>
+            </ul>
+          </nav>
         </>
-
       )}
     </header>
   );
