d632d14a799b2c2070038619c13ad502a25962cd
phihash <insharp0220@gmail.com>
2023-08-24 19:47:44 +0900

    -

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useId } from "react";
 
 interface CategoryProps {
   categoryName: string;
@@ -11,7 +12,13 @@ const Category: React.FC<CategoryProps> = ({
   setDramaCategories,
   dramaCategories,
 }) => {
+  const checkId = useId();
   const handleCategoriesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // if (e.target.checked) {
+    //   setSelectedCheckBox([...selectedCheckBox, choice])
+    // } else {
+    //   setSelectedCheckBox(selectedCheckBox.filter(item => item !== choice))
+    // }
     setDramaCategories([e.target.value, ...dramaCategories]);
     console.log(dramaCategories);
   };
@@ -19,13 +26,13 @@ const Category: React.FC<CategoryProps> = ({
     <div className="w-5/12 text-center">
       <input
         type="checkbox"
-        id={categoryName}
+        id={checkId}
         value={categoryName}
         className="peer hidden"
         onChange={handleCategoriesChange}
       />
       <label
-        htmlFor={categoryName}
+        htmlFor={checkId}
         className="text-sm md:text-base select-none cursor-pointer rounded-lg border-2 border-sky-200 bg-sky-200 block
    py-3 px-6 font-bold text-white transition-colors duration-200 ease-in-out peer-checked:bg-sky-500 peer-checked:text-white peer-checked:border-sky-500 "
       >
